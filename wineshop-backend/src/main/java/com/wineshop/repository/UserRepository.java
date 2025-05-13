7acc26ebbe5c98b54a83c773a5c3986f7cf39c84
DanhPhuongNha-22130191 <22130191@st.hcmuaf.edu.vn>
2025-05-13 15:02:04 +0700

    Clear nhánh main

--- wineshop-backend/src/main/java/com/wineshop/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wineshop.repository;
-
-import com.wineshop.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByUsername(String userName);
-}
